25039ab40aeae9b6a2540175416246f9e26fee3f
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-01-29 07:53:08 +0100

    First stage - template + earlu stage of menu

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Ingridient } from '../shared/ingridient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingridients: Ingridient[] = [
+    new Ingridient('Apples', 5),
+    new Ingridient('Tomatos', 3)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
