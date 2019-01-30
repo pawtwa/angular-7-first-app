a6b902202f5c0bf8adbe4f0ea1a7ca1c409315e0
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-01-30 13:50:57 +0100

    Attribute & Structural Directive - DropDown Menu

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostListener, Input, ElementRef, Renderer2, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
