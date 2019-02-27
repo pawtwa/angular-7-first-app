48f5eb3cb924be0f2797901900bd10ab610b10ca
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-02-27 11:05:09 +0100

    Adding the shopping list form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { Ingridient } from 'src/app/shared/ingridient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,10 +12,13 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInput: ElementRef<HTMLInputElement>;
   @ViewChild('amountInput') amountInput: ElementRef<HTMLInputElement>;
 
-  nameInputError: boolean = false;
-  amountInputError: boolean = false;
+  nameInputError: boolean;
+  amountInputError: boolean;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {
+    this.nameInputError = false;
+    this.amountInputError = false;
+  }
 
   ngOnInit() {
   }
@@ -27,11 +31,20 @@ export class ShoppingEditComponent implements OnInit {
     this.shoppingListService.clearIngridients();
   }
 
+  onAddItem(form: FormGroup) {
+    const value = form.value;
+    const ingridient = new Ingridient(value.name, value.amount);
+    this.shoppingListService.addIngridient(ingridient);
+  }
+
   onAddClick(event: UIEvent) {
     this.nameInputError = false;
     this.amountInputError = false;
     if (this.nameInput.nativeElement.value.trim().length && this.amountInput.nativeElement.value.length) {
-      const newIngridient: Ingridient = new Ingridient(String(this.nameInput.nativeElement.value), Number(this.amountInput.nativeElement.value));
+      const newIngridient: Ingridient = new Ingridient(
+        String(this.nameInput.nativeElement.value),
+        Number(this.amountInput.nativeElement.value)
+      );
       this.shoppingListService.addIngridient(newIngridient);
       this.nameInput.nativeElement.value = null;
       this.amountInput.nativeElement.value = null;
