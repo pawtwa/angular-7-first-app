613f15a74c9e4ccff86e2f2db3b6531846eac9d0
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-02-28 13:21:43 +0100

    Adding ingridient controls to a form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -34,11 +34,20 @@ export class RecipeEditComponent implements OnInit {
   }
 
   private initForm() {
+    const ingridients = new FormArray([]);
+    if (this.editedRecipe.ingridients) {
+      for (let ingridient of this.editedRecipe.ingridients) {
+        ingridients.push(new FormGroup({
+          'name': new FormControl(ingridient['name']),
+          'amount': new FormControl(ingridient['amount']),
+        }));
+      }
+    }
     this.recipeForm = new FormGroup({
       'name': new FormControl(this.editedRecipe.name),
       'imagePath': new FormControl(this.editedRecipe.imagePath),
       'description': new FormControl(this.editedRecipe.description),
-      'ingridients': new FormArray([])
+      'ingridients': ingridients
     });
   }
 
