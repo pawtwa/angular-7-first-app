5631b670543c8a31593eaa3a3576c2eaa3612244
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-02-28 08:08:49 +0100

    Creating the template for the (reactive) recipe edit form & Creating the form for editing recipes & syncing html with the form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {RecipeService} from '../recipe.service';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,16 +12,37 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode: boolean;
+  editedRecipe: Recipe;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = !isNaN(this.id) && this.id >= 0;
+        if (this.editMode) {
+          this.editedRecipe = this.recipeService.getRecipeById(this.id);
+        }
       }
-    )
+    );
+    this.initForm();
   }
 
+  private initForm() {
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(this.editedRecipe.name),
+      'imagePath': new FormControl(this.editedRecipe.imagePath),
+      'description': new FormControl(this.editedRecipe.description),
+      'ingridients': new FormArray([])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 }
