ec49142857073fe919959481b7892ed872b8122a
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-03-11 06:44:54 +0100

    Importin rxjs operators in a single way

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs';
-import 'rxjs/RX';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import firebase_config from '../shared/firebase-pconf';
 import { Recipe } from '../recipes/recipe.model';
