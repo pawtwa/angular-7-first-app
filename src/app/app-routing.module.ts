3263d83fe4e64cc7bc53cf277108a5350c78db7a
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-03-12 07:16:03 +0100

    Preloading Lazy Loaded Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, NoPreloading } from '@angular/router';
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { HomeComponent } from "./core/home/home.component";
 import { AuthGuardService } from "./auth/auth-guard.service";
@@ -26,6 +26,7 @@ const appRoutes: Routes = [
     imports: [
         RouterModule.forRoot(appRoutes, {
             //useHash: true
+            preloadingStrategy: PreloadAllModules
         })
     ],
     exports: [
